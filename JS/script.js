9f689d07f2c29e2d3714fc0ecc455b43976b57a3
JD Patel <jaypatel3000k@gmail.com>
2025-06-17 15:45:59 +0530

    just awey hi
    just awey hi

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,4 @@
+// For Typing Effect
 const texts = ["Web development", "Electronics"];
 let currentTextIndex = 0;
 let isDeleting = false;
@@ -5,6 +6,7 @@ let currentText = "";
 const typingSpeed = 30; // Speed in milliseconds
 const pause = 1000; // Pause between typing and deleting
 
+// Typing Effect Function
 function type() {
     const targetText = texts[currentTextIndex];
     const displayElement = document.getElementById("type_effect");
@@ -29,4 +31,50 @@ function type() {
     }
 }
 
+// calling typing effect function
 document.addEventListener("DOMContentLoaded", type);
+
+// Adding About me page
+
+// NOT mastered this technique yet
+// let aboutme = document.querySelectorAll(".nav_btn");
+// console.log(aboutme)
+// aboutme.forEach(element => {
+// element.addEventListener("click", (e)=>{
+// console.log(e.currentTarget)
+// console.log("pressed")
+// })
+// });
+
+// Lengthy code but reasonalble right now 
+let myproject = document.querySelector("#myproject");
+let electronics = document.querySelector("#electronics");
+let aboutme = document.querySelector("#aboutme");
+let contact = document.querySelector("#contact");
+
+// My Project button
+myproject.addEventListener("click", () => {
+    console.log("Pressed myproject");
+})
+
+// Electrnoics button 
+electronics.addEventListener("click", () => {
+    console.log("Pressed electronics");
+})
+
+// About me button
+aboutme.addEventListener("click", () => {
+    console.log("Pressed aboutme");
+    let main_tag = document.querySelector("main");
+    main_tag.style.display = "none"
+})
+
+// Contact button
+contact.addEventListener("click", () => {
+    console.log("Pressed contact");
+    let main_tag = document.querySelector("main");
+    main_tag.style.display = "none"
+})
+
+
+
